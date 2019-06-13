f3697e565e9392d10bab486ce77634ac98da5a39
Thies Gehrmann <thiesgehrmann@Thiess-MacBook-Pro-9.local>
2019-06-13 19:59:20 +0200

    Added News submodule

--- TWB/__init__.py
+++ TWB/__init__.py
@@ -2,3 +2,6 @@ from .document_features import *
 from .xliff import XLIFF
 from .lsc import LSC
 from .country_languages import CountryLanguages
+from .news import news_articles
+
+from . import common
